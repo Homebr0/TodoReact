1d66898830f251fddd023817ea031ae6918bcc64
Jason Davila <jsndavila1@gmail.com>
2020-10-07 15:25:28 -0400

    Some styling done

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 export class Todoitem extends Component {
+    getStyle = () => {
+        return {
+            background: 'f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 
+            'line-through' : 'none'
+        }
+    }
+
+    markComplete(e) {
+        console.log(this.props)
+    }
+
+
     render() {
         return (
-            <div>
-                <p>{ this.props.todo.title }</p>
+            <div style = {this.getStyle()}>
+                <p>
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this)} /> {' '}
+                     { this.props.todo.title }
+                </p>
             </div>
         )
     }
 }
+
+Todoitem.propTypes = {
+    todo: PropTypes.array.isRequired
+  }
+
+  
 
 export default Todoitem
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import TodoItem from './Todoitem'
+import TodoItem from './Todoitem';
+import PropTypes from 'prop-types';
 
 class Todos extends Component {
+markComplete = () => {
+  console.log('Hello')
+}
+
   render() {
     return this.props.todos.map((todo) => (
-    <TodoItem key={todo.id} todo={todo} />
+    <TodoItem key={todo.id} todo={todo} markComplete={this.markComplete}/>
     ));
   }
+}
+
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
 }
 
 export default Todos;
